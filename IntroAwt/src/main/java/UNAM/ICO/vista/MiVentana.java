a92799665ed3008c16992d63e3b535b1add6190f
Fercho182820 <Fercho182820@icloud.com>
2026-04-23 13:48:07 -0600

    Carpeta ventana

---
+++ IntroAwt/src/main/java/UNAM/ICO/vista/MiVentana.java
@@ -0,0 +1,14 @@
+package UNAM.ICO.vista;
+
+import java.awt.*;
+
+public class MiVentana extends Frame {
+
+    public MiVentana() {
+        super("Mi ventana");
+        setSize(800,600);
+        setVisible(true);
+
+    }
+
+}
