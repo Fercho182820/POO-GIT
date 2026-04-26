1275ee3580097e809e6bfdcc4d0f881d1efded3f
Fercho182820 <Fercho182820@icloud.com>
2026-04-25 19:12:57 -0600

    codigo limpio

--- Tarea11/src/main/java/UNAM/ICO/Main.java
+++ Tarea11/src/main/java/UNAM/ICO/Main.java
@@ -10,31 +10,7 @@ import java.util.Scanner;
 public class Main {
     static void main() {
 
-       // CalculadoraIMC calculadoraIMC = new CalculadoraIMC();
-       // Scanner teclado = new Scanner(System.in);
-//
-//        try {
-//
-//            System.out.println("Ingrese su peso: ");
-//            calculadoraIMC.setPeso(teclado.nextDouble());
-//            System.out.println("Ingrese su altura: ");
-//            calculadoraIMC.setAltura(teclado.nextDouble());
-//            calculadoraIMC.calcularIMC();
-//
-//        }catch (InputMismatchException e){
-//            System.out.println("Error revisa tu entrada");
-//            System.out.println(e.getMessage());
-//        }catch (NullPointerException e){
-//            System.out.println("Objeto nulo Revisa tu entrada");
-//            System.out.println(e.getMessage());
-//        }catch (Exception e){
-//            System.out.println("Error desconocido");
-//            System.out.println(e.getMessage());
-//        }
-//        System.out.println("Su IMC es: "+ calculadoraIMC.getImc());
-
-
-        VentanaImc ventanaImc = new VentanaImc("Descubra su IMC",800,600);
+        VentanaImc ventanaImc = new VentanaImc("Descubra su IMC",12000,600);
 
 
 
