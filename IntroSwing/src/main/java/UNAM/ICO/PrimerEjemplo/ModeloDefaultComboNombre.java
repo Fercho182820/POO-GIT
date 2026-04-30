1c81bff956581c70bf8f5a563fc11725e47c811c
Fercho182820 <Fercho182820@icloud.com>
2026-04-30 13:43:59 -0600

    ventana dos avance

---
+++ IntroSwing/src/main/java/UNAM/ICO/PrimerEjemplo/ModeloDefaultComboNombre.java
@@ -0,0 +1,13 @@
+package UNAM.ICO.PrimerEjemplo;
+
+import java.util.ArrayList;
+
+public class ModeloDefaultComboNombre {
+
+    //public ModeloDefaultComboNombre(String[] nombres ) {
+       // super(nombres);
+    //}
+    //public ModeloDefaultComboNombre(){
+      //  super();
+    //}
+}
--- IntroSwing/src/main/java/UNAM/ICO/PrimerEjemplo/VentanaDos.java
+++ IntroSwing/src/main/java/UNAM/ICO/PrimerEjemplo/VentanaDos.java
@@ -15,6 +15,8 @@ public class VentanaDos extends JFrame {
 
     public VentanaDos(){
         super("Mi segunda ventana Swing");
+        setSize(800,600);
+
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         layout = new GridLayout(2,2);
         getContentPane().setLayout(layout);
@@ -22,13 +24,26 @@ public class VentanaDos extends JFrame {
         panel2 = new JPanel();
         panel3 = new JPanel();
         panel4 = new JPanel();
-        panel1.setBackground(Color.red);
-        panel2.setBackground(Color.CYAN);
-        panel3.setBackground(Color.green);
-        panel4.setBackground(Color.gray);
+        panel1.setBackground(new Color(22,32,104));
+        panel2.setBackground(new Color(0,0,0));
+        panel3.setBackground(new Color(34,55,78));
+        panel4.setBackground(new Color(55,33,88));
 
-        //etiqueta new JLabel("Soy una etiqueta");
-        //boton new JButton("Soy un boton");
+        etiqueta = new JLabel("Soy una etiqueta");
+        boton = new JButton("Soy un boton");
+
+
+        getContentPane().add(panel1,0);
+        getContentPane().add(panel2,1);
+        getContentPane().add(panel3,2);
+        getContentPane().add(panel4,3);
+
+
+
+
+
+
+        setVisible(true);
 
 
 
